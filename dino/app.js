eea6e3171aca65b4f84a34d81e3b840d0459b974
Luis <jrluis7@gmail.com>
2022-10-14 19:47:19 +0200

    Versión inicial de Pixel creator!

--- dino/app.js
+++ dino/app.js
@@ -14,6 +14,7 @@ let grid = query('#grid');
 let entradaAncho = query('#ancho');
 let entradaAlto = query('#alto');
 let btnCambiarTam = query('#cambiarTamano');
+let btnExport = query('#export');
 
 const grid_element = new Grid(10, 12, grid);
 
@@ -24,6 +25,13 @@ nodoColor.addEventListener('change', function () {
     grid_element.setColor(this.value);
 })
 
-
+btnExport.addEventListener( 'click' , function () {
+    preview.appendChild(grid_element.exportToCss())
+})
 
 color.addEventListener
+
+let testString1 = '0px none rgb(98, 98, 98)'
+let testString2 = 'rgba(0, 0, 0, 0.75) 10px 10px 5px 0px'
+console.log(testString1.match(/(?:[^\s\(\)]+|\([^\(\)]*\))+/g));
+console.log(testString2.match(/(?:[^\s\(\)]+|\([^\(\)]*\))+/g));
--- dino/modules/Grid.js
+++ dino/modules/Grid.js
@@ -4,10 +4,28 @@ export default class Grid {
     tablero = [];
     color = "red";
     constructor(x, y, element, color) {
+        this.size_num = 1;
+        this.size = "em";
         this.x = x;
         this.y = y;
         this.element = element;
         this.color = color ? color : "black";
+
+        this.isPainting = false;
+
+        const mouseDown = () => {
+            console.log('Ey, click!', this);
+            console.log(this.seleccionado)
+            this.isPainting = true;
+        }
+        const mouseUp = () => {
+            this.isPainting = false;
+        }
+
+        window.addEventListener( 'mousedown' , mouseDown)
+        window.addEventListener( 'mouseup' , mouseUp )
+
+
         console.log(x, y);
         this.tablero = new Array(this.y);
         for (let i = 0; i < this.tablero.length; i++) {
@@ -61,6 +79,30 @@ export default class Grid {
         this.fillTablero();
     }
 
+    exportToCss(){
+        let str = '';
+        for( let y = 0; y < this.y; y++ ){
+            for( let x = 0; x<this.x; x++){
+
+                if( this.tablero[y][x]!== "" ){
+                    str += `${(x+1)*this.size_num}${this.size} ${(y+1)*this.size_num}${this.size} ${this.tablero[y][x]},`
+                }
+
+
+            }
+        }   
+
+        str=str.slice(0, -1);
+
+        let div = document.createElement('div');
+        div.style.cssText = `
+            height:${this.size_num}${this.size};
+            width:${this.size_num}${this.size};
+            `;
+           div.style.boxShadow = str;
+        debugger
+        return div
+    }
 
 }
 
@@ -76,16 +118,22 @@ class Square {
             console.log('Ey, click!', this);
             console.log(this.seleccionado)
             let color = (!this.seleccionado ? this.grid.color : '');
-            this.nodo.style.cssText = `background-color:${color}`;
-            this.seleccionado = !this.seleccionado;
-            this.grid.markSquare(this.grid.color, this.y, this.x);
+            this.paint( color);
 
+
+        }
+        const mouse_move = () => {
+            console.log('Ey, click!', this);
+            console.log(this.seleccionado)
+           this.paint(this.grid.color);
+            console.log(this.grid)
         }
         this.seleccionado = false;
         let span = document.createElement('span')
         span.classList.add('square');
 
         span.addEventListener('click', click_);
+        span.addEventListener('mousemove', mouse_move);
         this.nodo = span;
 
     }
@@ -93,6 +141,15 @@ class Square {
         return this.nodo;
     }
 
+
+    paint( color ){
+        if(this.grid.isPainting){
+
+            this.nodo.style.cssText = `background-color:${color}`;
+            this.seleccionado = !this.seleccionado;
+            this.grid.markSquare(color, this.y, this.x);
+        }
+    }
 
 
 }
